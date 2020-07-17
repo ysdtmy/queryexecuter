3ad33cf7ef5ec78dfb9a1b26021e456854c875a8
tomoya yoshida <ysdtmy3856@gmail.com>
2020-07-18 04:45:43 +0900

    autopep8 formatting

--- QueryEngine.py
+++ QueryEngine.py
@@ -9,90 +9,89 @@ import psycopg2
 dbinfo = configparser.ConfigParser()
 dbinfo.read('./dbinfo.conf', encoding='utf-8')
 
+
 class QueryEngine:
 
     def __init__(self, qe, coninfo):
         self.qe = eval(qe + '()')
         self.coninfo = coninfo
-    
+
     def connect(self, *arg, **kwarg):
         self.con = self.qe.connect(self.coninfo)
         return True
-    
+
     def execute(self, sql, *arg, **kwarg):
         result = self.qe.execute(self.con, sql)
         return result
 
+
 class DbConnectionSkelton:
 
     def __init__(self):
-      self.connectiontype = self.__class__.__name__
-      print("Query Engine : " + self.connectiontype)
-    
+        self.connectiontype = self.__class__.__name__
+        print("Query Engine : " + self.connectiontype)
+
     def connect(self, coninfo):
-      raise NotImplementedError
+        raise NotImplementedError
 
     def execute(self, con, sql, *arg, **kwarg):
-      raise NotImplementedError
+        raise NotImplementedError
 
 
 class SampleDbDef(DbConnectionSkelton):
 
     def __init__(self):
         super().__init__()
-    
+
     def connect(self, coninfo):
         conobj = coninfo
         print('Create Connection Object')
         return conobj
-    
+
     def execute(self, con, sql, *arg, **kwarg):
         print('Connecting Object: ' + con)
         print('Query: ' + sql)
-        
+
 
 class Postgresql(DbConnectionSkelton):
 
     def __init__(self):
         super().__init__()
-    
+
     def connect(self):
         conn = psycopg2.connect(
-              host = self.coninfo['host']
-             ,port = self.coninfo['port']
-             ,user = self.coninfo['user']
-             ,password = self.coninfo['user']
-             ,database = self.coninfo['database'] 
+            host=self.coninfo['host'], port=self.coninfo['port'], user=self.coninfo[
+                'user'], password=self.coninfo['user'], database=self.coninfo['database']
         )
 
         return conn
-    
+
     def execute(self, con, sql, *arg, **kwarg):
         cur = con.cursur()
         result = cur.execute()
         rows = result.fetchall()
         return rows
+
 
 class Presto(DbConnectionSkelton):
 
     def __init__(self):
         super().__init__()
-    
+
     def connect(self):
         conn = prestodb.connect(
-              host = self.coninfo['host']
-             ,port = self.coninfo['port']
-             ,user = self.coninfo['user'] ,password = self.coninfo['user']
-             ,database = self.coninfo['database'] 
+            host=self.coninfo['host'], port=self.coninfo['port'], user=self.coninfo[
+                'user'], password=self.coninfo['user'], database=self.coninfo['database']
         )
 
         return conn
-    
+
     def execute(self, con, sql, *arg, **kwarg):
         cur = con.cursur()
         result = cur.execute()
         rows = result.fetchall()
         return rows
+
 
 class Spark(DbConnectionSkelton):
 
