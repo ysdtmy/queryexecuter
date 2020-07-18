74d17e013e9711de681a1409247c47f219441091
tomoya yoshida <ysdtmy3856@gmail.com>
2020-07-18 20:47:24 +0900

    Implementing main method and logger

--- queryexecuter.py
+++ queryexecuter.py
@@ -1,86 +1,119 @@
 # -*-conding utf-8-*-
 
-import prestodb
-import psycopg2
 import logging
 import configparser
 import os
 import argparse
+import json
+import collections
+import yaml
+from QueryEngine import QueryEngine
 
 
-formatter = '%(levelname)s : %(asctime)s : %(message)s'
+formatter = '%(levelname)s : %(name)s : %(asctime)s : %(message)s'
 logging.basicConfig(level=logging.INFO, format=formatter)
-
-# Directory
-SQLBATCHFILE = './sql_batch.conf'
-SQLDIR = './sql'
+logging.getLogger(__name__)
 
 
-def main(dryrun=False, engine='postgresql'):
+def main(batchfile, dryrun=False):
 
-    def _create_connection(engine):
-
-        try:
-            logging.info('DBINFO host : %s', dbinfo[engine]['host'])
-            logging.info('DBINFO port : %s', dbinfo[engine]['port'])
-            logging.info('DBINFO user : %s', dbinfo[engine]['user'])
-
-            if engine == 'presto':
-                conn = prestodb.dbapi.connect(
-                    host=dbinfo[engine]['host'], port=int(dbinfo[engine]['port']), user=dbinfo[engine]['user']
-                )
-
-        except KeyError:
-            logging.info('Not found engine %s', engine)
-            exit(1)
-
-        return conn
-
-    def _execute_query(conn, sql):
-        cur = conn.cursor()
-        result = cur.execute(sql)
-        row = cur.fetchall()
-
-        return result, row
-
-    def _execute_sql_from_file(conn, sqlfile, parse_dict, dryrun=False):
-        logging.info('EXECUTE SQLFILE : %s', sqlfile)
-        resultset = (None, None)
-        with open(os.path.join(SQLDIR, sqlfile), 'r') as f:
-            sql = f.read()
-            parsed_sql = _parse_query(sql, parse_dict)
-            logging.info('EXECUTE SQL : %s', parsed_sql)
-            if dryrun == False:
-                resultset = _execute_query(conn, parsed_sql)
-
-        return resultset
-
-    def _parse_query(sql, parse_dict):
-        parsed_sql = sql.format(**parse_dict)
+    def _parse_query(sql, parameterfile):
+        with open(parameterfile) as pf:
+            paramdict = json.load(pf)
+        parsed_sql = sql.format(**paramdict)
 
         return parsed_sql
 
-    def _create_parse_dict(inputtable, outputtable):
-        pass
+    def _parse_batchfile(batchfile):
+        with open(batchfile, 'r') as bf:
+            batchdict = json.load(bf)
+            defaultsetting = batchdict["DefaltSetting"]
+            executes = batchdict["Execute"]
+
+        return defaultsetting, executes
+
+    def _get_targetdbinfo(dbinfofile, dbinfo):
+        with open(dbinfofile, "r") as di:
+            dbs = yaml.safe_load(di)
+        dbdict = dbs[dbinfo]
+
+        return dbdict
+
+    def _get_query_from_file(sqlfile):
+        with open(sqlfile, 'r') as sqlf:
+            query = sqlf.read()
+
+        return query
+
+    def _overwritesetting(defaultsetting, execute):
+        defalultkeys = defaultsetting.keys()
+        for dkey in defalultkeys:
+            if dkey not in execute.keys():
+                execute[dkey] = defaultsetting[dkey]
+
+        return execute
 
     logging.info('DRYRUNMODE : %s', dryrun)
-    logging.info('QUERY ENGINE : %s', engine)
-    conn = _create_connection(engine)
-    parsed_dict = {'TBLNAME': 't_calendar'}
-    logging.info('BATCHFILE : %s', SQLBATCHFILE)
-    with open(SQLBATCHFILE, 'r') as f:
-        for i, sql in enumerate(f.readlines()):
-            sql = sql.rstrip(os.linesep)
-            logging.info('QUERYSTEP : %i', i + 1)
-            result, row = _execute_sql_from_file(
-                conn, sql, parsed_dict, dryrun)
-            logging.info('RESULT : %s', result)
-            logging.info('ROW : %s', row)
+    logging.info('BATCH FILE : %s', batchfile)
+
+    defautsetting, executes = _parse_batchfile(batchfile)
+
+    for step, execute in executes.items():
+        logging.info('QUERYSTEP : %i', int(step))
+        setting = _overwritesetting(defautsetting, execute)
+        dbinfofile = setting["DatabaseInfoFile"]
+        engine = setting["QueryEngine"]
+        dbinfo = setting["DatabaseInfo"]
+        sqlfile = setting["SQLFile"]
+
+        logging.info('DBinfofile : %s', dbinfofile)
+        logging.info('Engine : %s', engine)
+        logging.info('DBInfo : %s', dbinfo)
+        logging.info('SQLFile : %s', sqlfile)
+
+        parameter = setting["Parameter"] if "Parameter" in setting.keys(
+        ) else None
+        option = setting["Option"] if "Option" in setting.keys() else None
+
+        logging.info('ParameterFile : %s', parameter)
+        logging.info('Option : %s', option)
+
+        targetdbdict = _get_targetdbinfo(dbinfofile, dbinfo)
+
+        qe = QueryEngine(engine, targetdbdict)
+
+        query = _get_query_from_file(sqlfile)
+
+        if parameter != None:
+            query = _parse_query(query, parameter)
+
+        logging.info('Execute Query')
+        logging.info('\n' + query)
+
+        # Skip connection & execute if dryrun mode.
+        if dryrun:
+            continue
+
+        try:
+            qe.connect()
+        except Exception as e:
+            logging.error(e)
+            break
+
+        try:
+            result = qe.execute(query)
+        except Exception as e:
+            logging.error(e)
+            break
+
+        logging.info('RESULT : %s', result)
+
+    logging.info("END")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--dryrun', action='store_true', default=False)
-    parser.add_argument('--engine', '-e', default='postgresql')
+    parser.add_argument('--batchfile', '-f')
     args = parser.parse_args()
-    main(dryrun=args.dryrun, engine=args.engine)
+    main(batchfile=args.batchfile, dryrun=args.dryrun)
